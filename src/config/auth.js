4b470e97103ab3002aa91f9f068d4a99f4605482
Nivaldo Bagagi Jr <bagagi.junior@gmail.com>
2019-12-19 00:19:52 -0300

    Desafio Módulo 02 - Bootcamp GoStack 9

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '980454f0b03e7771add10274e03c0479',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Niva' });
+});
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
+
+export default routes;
